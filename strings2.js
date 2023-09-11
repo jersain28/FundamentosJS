b64e08d87073ad03d0c7e2f68e434b2c5da42939
Jersain Marin Juarez <jersainmarin20679@gmail.com>
2023-09-11 15:58:41 -0600

    05 Metodos y Propiedades strings

---
+++ strings2.js
@@ -0,0 +1,10 @@
+// Metodos y propiedades Strings
+const text = 'Este es Un TeXt0 NicE';
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.charAt(2));
+console.log(text.indexOf('T'));
+console.log(text.startsWith('T'));
+console.log(text.endsWith('E'));
+console.log('*'.repeat(10));
