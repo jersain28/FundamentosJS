dc96650146a89389b92a6c53d337e6115151ee9c
Jersain Marin Juarez <jersainmarin20679@gmail.com>
2023-09-11 15:47:12 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,34 @@
+// Operadores Matematicos
+// + - * / % **
+// += -= *= /= %= **=
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 / 10);
+console.log(10 * 10);
+console.log(10 % 10);
+console.log(10 ** 10); // Potencia
+
+let num= 15;
+console.log(num += 5); // num = num + 5
+console.log(num -= 5);
+console.log(num *= 5);
+console.log(num /= 5);
+console.log(num %= 5);
+console.log(num **= 5);
+
+// Operadores Booleanos 
+// > < >= <= !== ===
+console.log(1 > 2);
+console.log(1 < 2);
+console.log(1 >= 2);
+console.log(1 <= 2);
+console.log(1 !== 2);
+console.log(1 === 2);
+
+// Operadores Logicos
+// and &&
+console.log(true && true);
+// or ||
+console.log( true || true);
+// not !
+console.log(!true);
